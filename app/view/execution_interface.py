4e1a6ddf3be13d4c68fe53549e699ea7127b2edd
Donaldduck8 <donaldgit@yandex.com>
2024-04-17 00:25:57 +0200

    A bit more yoinking and scroinking

--- app/view/execution_interface.py
+++ app/view/execution_interface.py
@@ -1,27 +1,23 @@
 # coding:utf-8
 import logging
 import threading
-from enum import Enum
-from typing import Union
 
-from PyQt5.QtCore import (QEvent, QPropertyAnimation, QRectF, QSize, Qt,
+from PyQt5.QtCore import (Qt,
                           pyqtSignal, QTime, QTimer)
-from PyQt5.QtGui import QColor, QIcon, QPainter, QResizeEvent
-from PyQt5.QtWidgets import (QFrame, QGraphicsOpacityEffect, QHBoxLayout,
-                             QLabel, QListWidgetItem, QVBoxLayout, QWidget)
-from qfluentwidgets import (FluentIconBase, FluentStyleSheet,
+from PyQt5.QtGui import QResizeEvent
+from PyQt5.QtWidgets import (QFrame, QHBoxLayout,
+                             QListWidgetItem, QVBoxLayout, QWidget)
+from qfluentwidgets import (FluentStyleSheet,
                             IndeterminateProgressRing, InfoBarIcon,
                             InfoBarPosition, ListWidget, TitleLabel)
-from qfluentwidgets.common.auto_wrap import TextWrap
-from qfluentwidgets.common.icon import FluentIcon as FIF
-from qfluentwidgets.common.icon import Theme, drawIcon, isDarkTheme
 from qfluentwidgets.common.style_sheet import FluentStyleSheet, themeColor
-from qfluentwidgets.components.widgets.button import TransparentToolButton
 from qfluentwidgets.components.widgets.list_view import ListWidget
 from qfluentwidgets import SubtitleLabel, Dialog
 
 from .. import installation_steps, utils
 from ..common.style_sheet import StyleSheet
+from .base_frame import BaseFrame
+from .listable_info_bar import InfoBar as ListableInfoBar
 
 
 class ListWidgetLogHandler(logging.Handler):
@@ -35,273 +31,12 @@ class ListWidgetLogHandler(logging.Handler):
         self.widget.bottomListView.listWidget.scrollToBottom()
 
 
-class Frame(QFrame):
-
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
-        self.hBoxLayout = QHBoxLayout(self)
-        self.hBoxLayout.setContentsMargins(4, 8, 4, 8)
-
-        self.setObjectName('frame')
-        StyleSheet.VIEW_INTERFACE.apply(self)
-
-    def addWidget(self, widget):
-        self.hBoxLayout.addWidget(widget)
-
-
-class ListFrame(Frame):
+class ListFrame(BaseFrame):
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.listWidget = CustomListWidget(self)
         self.addWidget(self.listWidget)
-
-
-class InfoBarIcon(FluentIconBase, Enum):
-    """ Info bar icon """
-
-    INFORMATION = "Info"
-    SUCCESS = "Success"
-    WARNING = "Warning"
-    ERROR = "Error"
-
-    def path(self, theme=Theme.AUTO):
-        if theme == Theme.AUTO:
-            color = "dark" if isDarkTheme() else "light"
-        else:
-            color = theme.value.lower()
-
-        return f':/qfluentwidgets/images/info_bar/{self.value}_{color}.svg'
-
-
-class InfoBarPosition(Enum):
-    """ Info bar position """
-    TOP = 0
-    BOTTOM = 1
-    TOP_LEFT = 2
-    TOP_RIGHT = 3
-    BOTTOM_LEFT = 4
-    BOTTOM_RIGHT = 5
-    NONE = 6
-
-
-class InfoIconWidget(QWidget):
-    """ Icon widget """
-
-    def __init__(self, icon: InfoBarIcon, parent=None):
-        super().__init__(parent=parent)
-        self.setFixedSize(36, 36)
-        self.icon = icon
-
-    def paintEvent(self, e):
-        painter = QPainter(self)
-        painter.setRenderHints(QPainter.Antialiasing | QPainter.SmoothPixmapTransform)
-
-        rect = QRectF(10, 10, 15, 15)
-        if self.icon != InfoBarIcon.INFORMATION:
-            drawIcon(self.icon, painter, rect)
-        else:
-            drawIcon(self.icon, painter, rect, indexes=[0], fill=themeColor().name())
-
-
-class InfoBar(QFrame):
-    """ Information bar """
-
-    closedSignal = pyqtSignal()
-
-    def __init__(self, icon: Union[InfoBarIcon, FluentIconBase, QIcon, str], title: str, content: str,
-                 orient=Qt.Horizontal, isClosable=True, duration=1000, position=InfoBarPosition.TOP_RIGHT,
-                 parent=None):
-        """
-        Parameters
-        ----------
-        icon: InfoBarIcon | FluentIconBase | QIcon | str
-            the icon of info bar
-
-        title: str
-            the title of info bar
-
-        content: str
-            the content of info bar
-
-        orient: Qt.Orientation
-            the layout direction of info bar, use `Qt.Horizontal` for short content
-
-        isClosable: bool
-            whether to show the close button
-
-        duraction: int
-            the time for info bar to display in milliseconds. If duration is less than zero,
-            info bar will never disappear.
-
-        parent: QWidget
-            parent widget
-        """
-        super().__init__(parent=parent)
-        self.title = title
-        self.content = content
-        self.orient = orient
-        self.icon = icon
-        self.duration = duration
-        self.isClosable = isClosable
-        self.position = position
-
-        self.titleLabel = QLabel(self)
-        self.contentLabel = QLabel(self)
-        self.closeButton = TransparentToolButton(FIF.CLOSE, self)
-        self.iconWidget = InfoIconWidget(icon)
-
-        self.hBoxLayout = QHBoxLayout(self)
-        self.textLayout = QHBoxLayout() if self.orient == Qt.Horizontal else QVBoxLayout()
-        self.widgetLayout = QHBoxLayout() if self.orient == Qt.Horizontal else QVBoxLayout()
-
-        self.opacityEffect = QGraphicsOpacityEffect(self)
-        self.opacityAni = QPropertyAnimation(
-            self.opacityEffect, b'opacity', self)
-
-        self.lightBackgroundColor = None
-        self.darkBackgroundColor = None
-
-        self.__initWidget()
-
-    def __initWidget(self):
-        self.opacityEffect.setOpacity(1)
-        self.setGraphicsEffect(self.opacityEffect)
-
-        self.closeButton.setFixedSize(36, 36)
-        self.closeButton.setIconSize(QSize(12, 12))
-        self.closeButton.setCursor(Qt.PointingHandCursor)
-        self.closeButton.setVisible(self.isClosable)
-
-        self.__setQss()
-        self.__initLayout()
-
-        self.closeButton.clicked.connect(self.close)
-
-    def __initLayout(self):
-        # self.hBoxLayout.setContentsMargins(6, 6, 6, 6)
-        self.hBoxLayout.setSizeConstraint(QVBoxLayout.SetMinimumSize)
-        self.textLayout.setSizeConstraint(QHBoxLayout.SetMinimumSize)
-        self.textLayout.setAlignment(Qt.AlignTop)
-        self.textLayout.setContentsMargins(0, 8, 0, 8)
-
-        self.hBoxLayout.setSpacing(0)
-        # self.textLayout.setSpacing(5)
-
-        # add icon to layout
-        self.hBoxLayout.addWidget(self.iconWidget, 0, Qt.AlignTop | Qt.AlignLeft)
-
-        # add title to layout
-        self.textLayout.addWidget(self.titleLabel, 1, Qt.AlignTop)
-        self.titleLabel.setVisible(bool(self.title))
-
-        # add content label to layout
-        # if self.orient == Qt.Horizontal:
-        #     self.textLayout.addSpacing(7)
-
-        self.textLayout.addWidget(self.contentLabel, 1, Qt.AlignLeft | Qt.AlignTop)
-        self.contentLabel.setVisible(bool(self.content))
-        self.hBoxLayout.addLayout(self.textLayout, stretch=1)
-
-        # add widget layout
-        if self.orient == Qt.Horizontal:
-            self.hBoxLayout.addLayout(self.widgetLayout)
-            self.widgetLayout.setSpacing(10)
-        else:
-            self.textLayout.addLayout(self.widgetLayout)
-
-        # add close button to layout
-        if self.isClosable:
-            self.hBoxLayout.addSpacing(4)
-            self.hBoxLayout.addWidget(self.closeButton, 0, Qt.AlignTop | Qt.AlignLeft)
-
-        self._adjustText()
-
-    def __setQss(self):
-        self.titleLabel.setObjectName('titleLabel')
-        self.contentLabel.setObjectName('contentLabel')
-        if isinstance(self.icon, Enum):
-            self.setProperty('type', self.icon.value)
-
-        FluentStyleSheet.INFO_BAR.apply(self)
-
-    def __fadeOut(self):
-        """ fade out """
-        self.opacityAni.setDuration(200)
-        self.opacityAni.setStartValue(1)
-        self.opacityAni.setEndValue(0)
-        self.opacityAni.finished.connect(self.close)
-        self.opacityAni.start()
-
-    def _adjustText(self):
-        w = 900 if not self.parent() else (self.parent().width() - 50)
-
-        # adjust title
-        # chars = max(min(w / 10, 120), 30)
-        self.titleLabel.setText(TextWrap.wrap(self.title, 50, False)[0])
-
-        # adjust content
-        # chars = max(min(w / 9, 120), 30)
-        # self.contentLabel.setText(TextWrap.wrap(self.content, chars, False)[0])
-        self.adjustSize()
-
-    def addWidget(self, widget: QWidget, stretch=0):
-        """ add widget to info bar """
-        self.widgetLayout.addSpacing(6)
-        align = Qt.AlignTop if self.orient == Qt.Vertical else Qt.AlignVCenter
-        self.widgetLayout.addWidget(widget, stretch, Qt.AlignLeft | align)
-
-    def setCustomBackgroundColor(self, light, dark):
-        """ set the custom background color
-
-        Parameters
-        ----------
-        light, dark: str | Qt.GlobalColor | QColor
-            background color in light/dark theme mode
-        """
-        self.lightBackgroundColor = QColor(light)
-        self.darkBackgroundColor = QColor(dark)
-        self.update()
-
-    def eventFilter(self, obj, e: QEvent):
-        if obj is self.parent():
-            if e.type() in [QEvent.Resize, QEvent.WindowStateChange]:
-                self._adjustText()
-
-        return super().eventFilter(obj, e)
-
-    def closeEvent(self, e):
-        self.closedSignal.emit()
-        self.deleteLater()
-
-    def showEvent(self, e):
-        self._adjustText()
-        super().showEvent(e)
-
-        if self.parent():
-            self.parent().installEventFilter(self)
-
-    def paintEvent(self, e):
-        super().paintEvent(e)
-        
-        if self.lightBackgroundColor is None:
-            return
-
-        painter = QPainter(self)
-        painter.setRenderHints(QPainter.Antialiasing)
-        painter.setPen(Qt.NoPen)
-
-        if isDarkTheme():
-            painter.setBrush(self.darkBackgroundColor)
-        else:
-            painter.setBrush(self.lightBackgroundColor)
-
-        rect = self.rect().adjusted(1, 1, -1, -1)
-        painter.drawRoundedRect(rect, 6, 6)
-
-    def resizeEvent(self, a0) -> None:
-        super().resizeEvent(a0)
-        FluentStyleSheet.INFO_BAR.apply(self)
 
 
 class CustomListWidget(ListWidget):
@@ -320,7 +55,7 @@ class CustomListWidget(ListWidget):
     def add_infobar(self, title, content):
         item = QListWidgetItem()
 
-        infoBar = InfoBar(
+        infoBar = ListableInfoBar(
             icon=InfoBarIcon.SUCCESS,
             title=title,
             content=content,
